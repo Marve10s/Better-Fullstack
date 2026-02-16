206ee55aa4f2fa7d279fa66f12ae5535401a0e17
Marve10s <igrimanigroman@gmail.com>
2026-02-17 02:32:20 +0300

    route

--- apps/web/src/routeTree.gen.ts
+++ apps/web/src/routeTree.gen.ts
@@ -8,80 +8,80 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from './routes/__root'
-import { Route as NewRouteImport } from './routes/new'
-import { Route as IndexRouteImport } from './routes/index'
-import { Route as ApiPreviewRouteImport } from './routes/api/preview'
+import { Route as rootRouteImport } from "./routes/__root";
+import { Route as ApiPreviewRouteImport } from "./routes/api/preview";
+import { Route as IndexRouteImport } from "./routes/index";
+import { Route as NewRouteImport } from "./routes/new";
 
 const NewRoute = NewRouteImport.update({
-  id: '/new',
-  path: '/new',
+  id: "/new",
+  path: "/new",
   getParentRoute: () => rootRouteImport,
-} as any)
+} as any);
 const IndexRoute = IndexRouteImport.update({
-  id: '/',
-  path: '/',
+  id: "/",
+  path: "/",
   getParentRoute: () => rootRouteImport,
-} as any)
+} as any);
 const ApiPreviewRoute = ApiPreviewRouteImport.update({
-  id: '/api/preview',
-  path: '/api/preview',
+  id: "/api/preview",
+  path: "/api/preview",
   getParentRoute: () => rootRouteImport,
-} as any)
+} as any);
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-  '/new': typeof NewRoute
-  '/api/preview': typeof ApiPreviewRoute
+  "/": typeof IndexRoute;
+  "/new": typeof NewRoute;
+  "/api/preview": typeof ApiPreviewRoute;
 }
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-  '/new': typeof NewRoute
-  '/api/preview': typeof ApiPreviewRoute
+  "/": typeof IndexRoute;
+  "/new": typeof NewRoute;
+  "/api/preview": typeof ApiPreviewRoute;
 }
 export interface FileRoutesById {
-  __root__: typeof rootRouteImport
-  '/': typeof IndexRoute
-  '/new': typeof NewRoute
-  '/api/preview': typeof ApiPreviewRoute
+  __root__: typeof rootRouteImport;
+  "/": typeof IndexRoute;
+  "/new": typeof NewRoute;
+  "/api/preview": typeof ApiPreviewRoute;
 }
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/new' | '/api/preview'
-  fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/new' | '/api/preview'
-  id: '__root__' | '/' | '/new' | '/api/preview'
-  fileRoutesById: FileRoutesById
+  fileRoutesByFullPath: FileRoutesByFullPath;
+  fullPaths: "/" | "/new" | "/api/preview";
+  fileRoutesByTo: FileRoutesByTo;
+  to: "/" | "/new" | "/api/preview";
+  id: "__root__" | "/" | "/new" | "/api/preview";
+  fileRoutesById: FileRoutesById;
 }
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute
-  NewRoute: typeof NewRoute
-  ApiPreviewRoute: typeof ApiPreviewRoute
+  IndexRoute: typeof IndexRoute;
+  NewRoute: typeof NewRoute;
+  ApiPreviewRoute: typeof ApiPreviewRoute;
 }
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/new': {
-      id: '/new'
-      path: '/new'
-      fullPath: '/new'
-      preLoaderRoute: typeof NewRouteImport
-      parentRoute: typeof rootRouteImport
-    }
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexRouteImport
-      parentRoute: typeof rootRouteImport
-    }
-    '/api/preview': {
-      id: '/api/preview'
-      path: '/api/preview'
-      fullPath: '/api/preview'
-      preLoaderRoute: typeof ApiPreviewRouteImport
-      parentRoute: typeof rootRouteImport
-    }
+    "/new": {
+      id: "/new";
+      path: "/new";
+      fullPath: "/new";
+      preLoaderRoute: typeof NewRouteImport;
+      parentRoute: typeof rootRouteImport;
+    };
+    "/": {
+      id: "/";
+      path: "/";
+      fullPath: "/";
+      preLoaderRoute: typeof IndexRouteImport;
+      parentRoute: typeof rootRouteImport;
+    };
+    "/api/preview": {
+      id: "/api/preview";
+      path: "/api/preview";
+      fullPath: "/api/preview";
+      preLoaderRoute: typeof ApiPreviewRouteImport;
+      parentRoute: typeof rootRouteImport;
+    };
   }
 }
 
@@ -89,16 +89,17 @@ const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   NewRoute: NewRoute,
   ApiPreviewRoute: ApiPreviewRoute,
-}
+};
 export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>()
+  ._addFileTypes<FileRouteTypes>();
 
-import type { getRouter } from './router.tsx'
-import type { createStart } from '@tanstack/react-start'
-declare module '@tanstack/react-start' {
+import type { createStart } from "@tanstack/react-start";
+
+import type { getRouter } from "./router.tsx";
+declare module "@tanstack/react-start" {
   interface Register {
-    ssr: true
-    router: Awaited<ReturnType<typeof getRouter>>
+    ssr: true;
+    router: Awaited<ReturnType<typeof getRouter>>;
   }
 }
